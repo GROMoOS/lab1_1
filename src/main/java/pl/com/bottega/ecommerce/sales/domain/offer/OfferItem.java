2982c0b9a2a2233017ec8e1ca30bbab67296b50c
student <student@L407-2.carbon.cti.p.lodz.pl>
2022-03-07 17:53:10 +0100

    move Type associated fields form OfferItem to new class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
@@ -15,24 +15,16 @@ package pl.com.bottega.ecommerce.sales.domain.offer;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class OfferItem {
+public class OfferItem extends Product {
 
-    // product
-    private String productId;
 
-    private BigDecimal productPrice;
-
-    private String productName;
-
-    private Date productSnapshotDate;
-
-    private String productType;
 
     private int quantity;
 
     private BigDecimal totalCost;
 
     private String currency;
+    private BigDecimal productPrice;
 
     // discount
     private String discountCause;
@@ -46,11 +38,8 @@ public class OfferItem {
 
     public OfferItem(String productId, BigDecimal productPrice, String productName, Date productSnapshotDate,
             String productType, int quantity, BigDecimal discount, String discountCause) {
-        this.productId = productId;
+        super(productId, productName, productSnapshotDate, productType);
         this.productPrice = productPrice;
-        this.productName = productName;
-        this.productSnapshotDate = productSnapshotDate;
-        this.productType = productType;
 
         this.quantity = quantity;
         this.discount = discount;
@@ -64,24 +53,8 @@ public class OfferItem {
         this.totalCost = productPrice.multiply(new BigDecimal(quantity)).subtract(discountValue);
     }
 
-    public String getProductId() {
-        return productId;
-    }
-
     public BigDecimal getProductPrice() {
         return productPrice;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public Date getProductSnapshotDate() {
-        return productSnapshotDate;
-    }
-
-    public String getProductType() {
-        return productType;
     }
 
     public BigDecimal getTotalCost() {

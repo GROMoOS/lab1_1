6bc5d476fa2cf9cd83dea3c9a48ce05d4c44fdd1
Arieli1512 <kubikapb@gmail.com>
2022-03-07 18:18:18 +0100

    Dodanie setterów i getterów do klasy

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -25,5 +25,36 @@ public class Product {
         return price;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getSnapshotDate() {
+        return snapshotDate;
+    }
+
+    public void setSnapshotDate(Date snapshotDate) {
+        this.snapshotDate = snapshotDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
 

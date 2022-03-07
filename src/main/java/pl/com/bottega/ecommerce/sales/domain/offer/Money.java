5a4197904a6dbcd051b5d50cd146f3702ab8b265
Konrad Basta <228685@edu.p.lodz.pl>
2022-03-07 18:31:35 +0100

    Overrited equals and hashCode in Money

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -30,4 +30,35 @@ public class Money {
 	public void setCurrency(String currency) {
 		this.currency = currency;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (obj == null)
+			return false;
+
+		if (getClass() != obj.getClass())
+			return false;
+
+		Money other = (Money) obj;
+
+		if(!amount.equals(other.amount))
+			return false;
+
+		if(!currency.equals(other.currency))
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (amount == null ? 0 : amount.hashCode());
+		result = prime * result + (currency == null ? 0 : currency.hashCode());
+		return result;
+	}
 }

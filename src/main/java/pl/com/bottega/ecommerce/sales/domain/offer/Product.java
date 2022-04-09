9da3b8e9335f64c3651f453d1142ff6ff468713c
Oskar Schilling <80629512+GROMoOS@users.noreply.github.com>
2022-04-09 14:48:22 +0200

    create Product class, integrate Product into OfferItem class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,91 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+public class Product {
+
+    private String id;
+    private BigDecimal price;
+    private String name;
+    private Date snapshotDate;
+    private String type;
+
+    public Product(String id, BigDecimal price, String name, Date snapshotDate, String type) {
+        this.id = id;
+        this.price = price;
+        this.name = name;
+        this.snapshotDate = snapshotDate;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getSnapshotDate() {
+        return snapshotDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Product product = (Product) o;
+        if (id == null) {
+            if (product.id != null) {
+                return false;
+            }
+        } else if (!id.equals(product.id)) {
+            return false;
+        }
+        if (price == null) {
+            if (product.price != null) {
+                return false;
+            }
+        } else if (!price.equals(product.price)) {
+            return false;
+        }
+        if (name == null) {
+            if (product.name != null) {
+                return false;
+            }
+        } else if (!name.equals(product.name)) {
+            return false;
+        }
+        if (snapshotDate == null) {
+            if (product.snapshotDate != null) {
+                return false;
+            }
+        } else if (!snapshotDate.equals(product.snapshotDate)) {
+            return false;
+        }
+        if (type == null) {
+            if (product.type != null) {
+                return false;
+            }
+        } else if (!type.equals(product.type)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(id, price, name, snapshotDate, type);
+    }
+}
